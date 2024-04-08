bb1c72844f6a4518dfd2ae4d5560cf65f339125d
royvardhan <royvardhan.official@gmail.com>
2024-04-08 12:14:11 +0530

    chore: sync

--- src/sync/merkle.cjs
+++ src/sync/merkle.cjs
@@ -1,26 +1,24 @@
 const { AbiCoder } = require("ethers");
 const keccack256 = require("keccak256");
 const { MerkleTree } = require("merkletreejs");
-const getTokenIdDataCombimedWithTier = require("./merkle.cjs");
 
-// async function getMerkleRoot(chainId) {
-//   const data = await getTokenIdDataCombimedWithTier(chainId);
-//   const leaves = Object.keys(data).map((tokenId) => {
-//     return encodeLeaf(tokenId, data[tokenId], data[tokenId].tier);
-//   });
-//   const tree = new MerkleTree(leaves, keccack256, { sortPairs: true });
-//   const root = tree.getHexRoot();
-//   console.log("root:", root);
-//   return root;
-// }
+const getCombinedPoints = require("./utils.cjs");
 
-// function encodeLeaf(tokenId, points, tier) {
-//   const encoder = AbiCoder.defaultAbiCoder();
-//   const leaf = encoder.encode(
-//     ["uint256", "uint256", "uint256"],
-//     [tokenId, points]
-//   );
-//   return leaf;
-// }
+async function getMerkleRoot(chainId) {
+  const data = await getCombinedPoints(chainId);
+  const leaves = Object.keys(data).map((tokenId) => {
+    return encodeLeaf(tokenId, data[tokenId]);
+  });
+  const tree = new MerkleTree(leaves, keccack256, { sortPairs: true });
+  const root = tree.getHexRoot();
+  console.log("root:", root);
+  return root;
+}
+
+function encodeLeaf(tokenId, points) {
+  const encoder = AbiCoder.defaultAbiCoder();
+  const leaf = encoder.encode(["uint256", "uint256"], [tokenId, points]);
+  return leaf;
+}
 
 getMerkleRoot(43114).catch((error) => console.error(error));
--- src/sync/utils.cjs
+++ src/sync/utils.cjs
@@ -2,9 +2,6 @@ require("dotenv").config();
 const postgres = require("postgres");
 const sql = postgres(process.env.DATABASE_URL);
 
-const weeklyEntitiesInMonth = 4;
-const weeklyEntitiesInYear = weeklyEntitiesInMonth * 12;
-
 async function getAllPoints(chainId) {
   const userIdPoint = await sql`
     SELECT "userDataId",  "points"
@@ -69,97 +66,4 @@ async function getCombinedPoints(chainId) {
   return combinedPointByTokenId;
 }
 
-async function getTokenIdData(chainId) {
-  const tokenIdData = await sql`
-    SELECT *
-    FROM public."TokenIdData"
-    WHERE "chainId" = ${chainId}
-    ;`;
-}
-
-async function getTokenIdDataWeekly(chainId, tokenId) {
-  const tokenIdDataWeekly = await sql`
-  SELECT *
-  FROM public."TokenIdDataWeekly"
-  WHERE "chainId" = ${chainId}
-  AND "tokenId" = ${tokenId}
-  ORDER BY "lastUpdated" DESC
-  ;`;
-
-  return tokenIdDataWeekly;
-}
-
-async function getTokenIdDataCombimed(chainId) {
-  const tokenIdData = await getTokenIdData(chainId);
-
-  const tokenIdDataCombined = tokenIdData.reduce((acc, data) => {
-    const tokenId = data.tokenId;
-    if (!acc[tokenId]) {
-      acc[tokenId] = {
-        pointsEarned: 0,
-        pointsClaimed: 0,
-        pointsSpent: 0,
-      };
-    }
-    acc[tokenId].pointsEarned += parseInt(data.pointsEarned);
-    acc[tokenId].pointsClaimed += parseInt(data.pointsClaimed);
-    acc[tokenId].pointsSpent += parseInt(data.pointsSpent);
-    return acc;
-  }, {});
-
-  return tokenIdDataCombined;
-}
-
-// Lets introduce a tier system to the points
-// Example, the top 0.01% of the users get the "Obsidian" tier
-// The top 0.05% get the "Diamond" tier
-// The top 0.2% get the "Platinum" tier
-// The top 1% get the "Gold" tier
-// The top 5% get the "Silver" tier
-// Remaining get the "Basic" tier
-function getTier(tokenIdDataCombined, userTokenId) {
-  // Extract data into an array for sorting
-  const tokenIdData = Object.keys(tokenIdDataCombined).map((tokenId) => {
-    const { pointsEarned, pointsClaimed, pointsSpent } =
-      tokenIdDataCombined[tokenId];
-    return { tokenId, pointsEarned, pointsClaimed, pointsSpent };
-  });
-
-  // Sort tokenIdData by (pointsEarned - pointsSpent) in descending order
-  tokenIdData.sort((a, b) => {
-    const totalPointsA = a.pointsEarned - a.pointsSpent;
-    const totalPointsB = b.pointsEarned - b.pointsSpent;
-    return totalPointsB - totalPointsA;
-  });
-
-  // Calculate the percentile of the user
-  const userIndex = tokenIdData.findIndex(
-    (data) => data.tokenId === userTokenId
-  );
-  const totalUsers = tokenIdData.length;
-  const percentile = ((totalUsers - userIndex) / totalUsers) * 100;
-
-  // Determine the tier based on the percentile
-  if (percentile <= 0.01) {
-    return 5;
-  } else if (percentile <= 0.05) {
-    return 4;
-  } else if (percentile <= 0.2) {
-    return 3;
-  } else if (percentile <= 1) {
-    return 2;
-  } else if (percentile <= 5) {
-    return 1;
-  } else {
-    return 0;
-  }
-}
-
-async function getTokenIdDataCombimedWithTier(chainId, userTokenId) {
-  const tokenIdDataCombined = await getTokenIdDataCombimed(chainId);
-  const tier = getTier(tokenIdDataCombined, userTokenId);
-
-  return { ...tokenIdDataCombined, tier };
-}
-
-module.exports = getTokenIdDataCombimedWithTier;
+module.exports = getCombinedPoints;
